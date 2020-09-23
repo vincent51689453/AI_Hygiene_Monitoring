c547cd54ef566f852b428f9e5a11900ecfd61439
vincent51689453 <vincent56189453@gmail.com>
2020-09-23 15:18:59 +0800

    test publisher

--- 001-JetsonTX2_Subscriber/001-QE_Hand_Hygiene/path_analyzer/MQTT_Fake_Publisher.py
+++ 001-JetsonTX2_Subscriber/001-QE_Hand_Hygiene/path_analyzer/MQTT_Fake_Publisher.py
@@ -2,19 +2,50 @@
 
 import paho.mqtt.client as mqtt
 import time
+import argparse
+
+parser = argparse.ArgumentParser()
+
+#Incompliance patient
+parser.add_argument('-a')
+
+#Compliance patient
+parser.add_argument('-b')
+
+#Incompliance surrondings
+parser.add_argument('-c')
+
+#Compliance surrondings
+parser.add_argument('-d')
+parameters = parser.parse_args()
+
+w = int(parameters.a)
+x = int(parameters.b)
+y = int(parameters.c)
+z = int(parameters.d)
+
+total_count = w + x + y + z
+if(total_count>0):
+    sucess_rate = (x + z)/total_count
+else:
+    sucess_rate = 0
+total_success = x + z
+total_fail = w + y
+
+print("a:{} b:{} c:{} d:{}".format(w,x,y,z))
 # This is the Publisher
 
 client = mqtt.Client()
 client.connect("localhost",1883)
 
-mqtt_message =' { "total_count" :' + str(3)+ \
-    ', "valid_patient_counter" :'+ str(1)+ \
-    ', "invalid_patient_counter" :'+str(1)+\
-    ', "valid_exit_counter":'+str(1)+\
-    ', "invalid_exit_counter":'+str(0) +\
-    ', "success_rate":'+str(66) +\
-    ', "total_sucess":'+str(2) +\
-    ', "total_fail":'+str(1) + '}'
+mqtt_message =' { "total_count" :' + str(total_count)+ \
+    ', "valid_patient_counter" :'+ str(x)+ \
+    ', "invalid_patient_counter" :'+str(w)+\
+    ', "valid_exit_counter":'+str(z)+\
+    ', "invalid_exit_counter":'+str(y) +\
+    ', "success_rate":'+str(sucess_rate) +\
+    ', "total_sucess":'+str(total_success) +\
+    ', "total_fail":'+str(total_fail) + '}'
 print("[INFO] MQTT Message:",mqtt_message)
 client.publish("MDSSCC/AIHH/gui_dashboard", mqtt_message)
 client.loop_stop()
